8a5bde7f7f4eb24a9f24345af6933c16b5474511
Junou Lee <junoulee@gmail.com>
2022-12-28 07:36:16 +0000

    assigns an entryId property to the new object, taken from the nextEntryId property of the data model.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var photoPreview = document.querySelector('#photoURL');
+var photoPreview = document.querySelector('#photo-url');
+var placeHolder = document.querySelector('.placeholder');
 var $form = document.querySelector('.journal-form');
 
 function showPic(event) {
-  photoPreview.setAttribute('src', event.target.value);
-
+  placeHolder.setAttribute('src', event.target.value);
 }
 
 photoPreview.addEventListener('input', showPic);
@@ -11,11 +11,10 @@ photoPreview.addEventListener('input', showPic);
 function storeValues(event) {
   event.preventDefault();
   var titleValue = $form.elements.title.value;
-
   var urlValue = $form.elements.url.value;
   var notesValue = $form.elements.notes.value;
-  var object = { title: titleValue, url: urlValue, notes: notesValue };
-  return object;
-}
+  var fieldEntries = { title: titleValue, url: urlValue, notes: notesValue, entryId: '' };
+  return fieldEntries;
 
+}
 $form.addEventListener('submit', storeValues);
