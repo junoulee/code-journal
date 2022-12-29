cbd9a9216bf551fd6375e14c8b3534ae5b4ad7c5
Junou Lee <junoulee@gmail.com>
2022-12-29 07:50:56 +0000

    Remove the example HTML entries from index.html once the user's entries are successfully being appended to the DOM

--- js/main.js
+++ js/main.js
@@ -24,3 +24,54 @@ function storeValues(event) {
 }
 
 $form.addEventListener('submit', storeValues);
+
+var hiddenDiv = document.querySelector('.journal-entries');
+
+function renderEntry(entry) {
+
+  var bullets = document.createElement('li');
+  bullets.classList.add('saved-entries');
+  hiddenDiv.appendChild(bullets);
+
+  var listRow = document.createElement('div');
+  listRow.classList.add('row');
+  bullets.appendChild(listRow);
+
+  var columnHalfOne = document.createElement('div');
+  columnHalfOne.classList.add('column-half');
+  listRow.appendChild(columnHalfOne);
+
+  var savedPhoto = document.createElement('img');
+  savedPhoto.classList.add('saved-photo');
+  columnHalfOne.appendChild(savedPhoto);
+
+  var columnHalfTwo = document.createElement('div');
+  columnHalfTwo.classList.add('column-half');
+  listRow.appendChild(columnHalfTwo);
+
+  var nameTitle = document.createElement('h3');
+  nameTitle.classList.add('name-title');
+  var titleValueText = document.createTextNode(data.entries.title);
+  columnHalfTwo.appendChild(nameTitle);
+  nameTitle.appendChild(titleValueText);
+
+  var savedText = document.createElement('p');
+  savedText.classList.add('saved-text');
+  var notesValueText = document.createTextNode(data.entries.notes);
+  columnHalfTwo.appendChild(savedText);
+  savedText.appendChild(notesValueText);
+
+  return bullets; // or return bullets
+
+}
+
+function dataLoop() {
+
+  for (var i = 0; i < data.entries.length; i++) {
+    var entriesDOM = renderEntry(data.entries[i]);
+    hiddenDiv.appendChild(entriesDOM);
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', dataLoop);
